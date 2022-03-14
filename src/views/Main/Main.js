4c50a6171a87fcf38085c4601ff9bce55100e195
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-14 16:51:07 -0700

    deleted try

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -4,22 +4,16 @@ import getBlogs from '../../services/Blogs';
 
 export default function Main() {
   const [blogs, setBlogs] = useState([]);
-  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const resp = await getBlogs();
-        setBlogs(resp);
-      } catch (e) {
-        setErrorMessage('Something went wrong. Refresh the page');
-      }
+      const resp = await getBlogs();
+      setBlogs(resp);
     };
     fetchData();
   }, []);
   return (
     <>
-      <p className="error">{errorMessage}</p>
       <div className="main">
         {blogs.map((blog) => (
           <BlogCard key={blog.title} {...blog} />

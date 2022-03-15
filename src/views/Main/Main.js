ce53c70fe8ce673acd983450effd993f4f57bec5
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-14 17:20:47 -0700

    Added loading but is not working yet

--- src/views/Main/Main.js
+++ src/views/Main/Main.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import BlogCard from '../../components/BlogCard/BlogCard';
 import getBlogs from '../../services/Blogs';
+import './Main.css';
 
 export default function Main() {
   const [blogs, setBlogs] = useState([]);
-
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     const fetchData = async () => {
       const resp = await getBlogs();
       setBlogs(resp);
+      setLoading(false);
     };
     fetchData();
   }, []);
+  if (loading) return <span className="loader"></span>;
   return (
     <>
       <div className="main">

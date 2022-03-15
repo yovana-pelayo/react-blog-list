11f4f40482496f1fd023fa0205d0e7c363f61413
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-03-14 17:26:02 -0700

    deleted console.log

--- src/services/Blogs.js
+++ src/services/Blogs.js
@@ -2,6 +2,5 @@ import { checkError, client } from './client';
 
 export default async function getBlogs() {
   const resp = await client.from('blogs').select('*');
-  console.log('first', resp);
   return checkError(resp);
 }
